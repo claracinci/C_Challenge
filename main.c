9c143d161bc79873c7342a469014e1e431e0cab6
claracinci <cincinato1109@gmail.com>
2024-10-23 22:25:42 -0300

    Mostra instruções e gera questões base

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define MAX_QUESTOES 3  // número total de questões
 
 void exibir_wellcome() {
     printf("\n");
@@ -17,6 +18,38 @@ void exibir_wellcome() {
     printf("\033[0m"); // tira o negrito
 }
 
+typedef struct {
+    char pergunta[300];
+    char altA[50];
+    char altB[50];
+    char altC[50];
+    char altD[50];
+    char resposta_correta; 
+} Pergunta;
+
+// função para gerar questões em um arquivo de texto
+int questao_txt() {
+    Pergunta perguntas[MAX_QUESTOES] = {
+        {"1. Quanto é 1+1?", "A. 2", "B. 3", "C. 4", "D. 5", 'A'},
+        {"2. Quanto é 2+1?", "A. 2", "B. 3", "C. 4", "D. 5", 'B'},
+        {"3. Quanto é 2+2?", "A. 2", "B. 3", "C. 4", "D. 5", 'C'}
+    };
+
+    FILE *fp = fopen("questoes.txt", "w");
+    if (fp == NULL) {
+        printf("Erro ao escrever no questoes.txt\n");
+        return 1;
+    }
+
+    for (int i = 0; i < MAX_QUESTOES; i++) {
+        fprintf(fp, "%s\n%s\n%s\n%s\n%s\n%c\n", perguntas[i].pergunta, perguntas[i].altA, perguntas[i].altB, perguntas[i].altC, perguntas[i].altD, perguntas[i].resposta_correta);
+    }
+
+    fclose(fp);
+    return 0;
+}
+
+// Função para exibir instruções do jogo
 void exibir_instrucoes() {
     printf("Instruções do Jogo:\n");
     printf("Olá, seja bem-vindo ao C Learning Challenge! Você será apresentado a uma série de perguntas.\n");
@@ -25,38 +58,9 @@ void exibir_instrucoes() {
     printf("Boa sorte!\n\n");
 }
 
-void mostrar_menu() {
-    int opcao;
-
-    do {
-        printf("\n");
-        printf("====================\n");
-        printf("|   Menu do Jogo    |\n");
-        printf("|1. Iniciar Jogo    |\n");
-        printf("|2. Instruções      |\n");
-        printf("|3. Sair            |\n");
-        printf("====================\n");
-        printf("Escolha uma opção: ");
-        scanf("%d", &opcao);
-
-        switch (opcao) {
-            case 1:
-                printf("Iniciando o jogo...\n");
-                break;
-            case 2:
-                exibir_instrucoes();
-                break;
-            case 3:
-                printf("Saindo do jogo. Até mais!\n");
-                break;
-            default:
-                printf("Opção inválida! Tente novamente.\n");
-        }
-    } while (opcao != 3);
-}
-
 int main() {
     exibir_wellcome();
-    mostrar_menu();
+    questao_txt();
+    exibir_instrucoes(); 
     return 0;
 }
